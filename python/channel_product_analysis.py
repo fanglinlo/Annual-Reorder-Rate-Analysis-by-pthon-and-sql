ac577fef06263c728dc522064914848a702fd76b
fanglinlo <38466426+fanglinlo@users.noreply.github.com>
2023-02-13 18:10:32 +0800

    Create channel_product_analysis.py
    combine store channel type and product type analysis

---
+++ python/channel_product_analysis.py
@@ -0,0 +1,75 @@
+import pandas as pd
+pd.set_option('display.max_columns', None)
+
+customers = pd.read_csv('./綠藤企業案例_題目解析與資料集/table 1. Customers.csv')
+orders = pd.read_csv('./綠藤企業案例_題目解析與資料集/table 2. Orders.csv')
+orderDetails = pd.read_csv('./綠藤企業案例_題目解析與資料集/table 3. OrderDetails.csv')
+products = pd.read_csv('./綠藤企業案例_題目解析與資料集/table 4. Products.csv')
+channels = pd.read_csv('./綠藤企業案例_題目解析與資料集/table 5. Channels.csv')
+
+ord_cnt = orders.groupby(["CustomerId", "TransactionYear"])["OrderId"].count().reset_index()
+ord_cnt = ord_cnt.rename(columns={"OrderId": "buy_count"})
+
+
+orders_core = customers.merge(orders, left_on=['CustomerId', 'FirstTransactionDate'],right_on=['CustomerId','TransactionDate'],
+                              how='left')\
+    .merge(orderDetails, on='OrderId', how='left')\
+    .merge(products[products['ProductType'] == '核心產品'], on='ProductId', how='left').dropna(how='any')
+orders_core = orders_core[['CustomerId','ProductType']]
+
+orders_road = customers.merge(orders, left_on=['CustomerId', 'FirstTransactionDate'],right_on=['CustomerId','TransactionDate'],
+                              how='left')\
+    .merge(orderDetails, on='OrderId', how='left')\
+    .merge(products[products['ProductType'] == '帶路產品'], on='ProductId', how='left').dropna(how='any')\
+    .drop_duplicates(subset=['CustomerId', 'FirstTransactionYear'])
+orders_road = orders_road[['CustomerId','ProductType']]
+
+df = customers.merge(ord_cnt,
+                     left_on=['CustomerId', 'FirstTransactionYear'],
+                     right_on=['CustomerId', 'TransactionYear'],
+                     how='left')
+df = df.merge(orders_core, on='CustomerId', how='left')
+df = df.merge(orders_road, on='CustomerId', how='left', suffixes=('', '_road'))
+
+# 加入線上線下
+df = df.merge(channels, left_on='FirstChannel', right_on='Channel', how='left')
+
+
+df = df[['FirstTransactionYear','CustomerId','buy_count','ProductType','ProductType_road','ChannelType']]
+
+# new_ord
+new_ord = df.groupby(['FirstTransactionYear','ChannelType']).agg({'CustomerId': 'nunique'})\
+    .rename(columns={'CustomerId':'new_ord'})
+
+# rep_ord
+rep_ord = df[df['buy_count']>=2]\
+    .groupby(['FirstTransactionYear','ChannelType'])\
+    .agg({'CustomerId': 'nunique'})\
+    .rename(columns={'CustomerId':'rep_ord'})
+
+#只買核心商品
+only_core = df[(df['ProductType'].notnull())&(df['ProductType_road'].isnull())]\
+    .groupby(['FirstTransactionYear','ChannelType']).agg({'CustomerId': 'nunique'})\
+    .rename(columns={'CustomerId': 'only_core'})
+
+#只買帶路商品
+only_road = df[(df['ProductType'].isnull())&(df['ProductType_road'].notnull())]\
+    .groupby(['FirstTransactionYear','ChannelType']).agg({'CustomerId': 'nunique'})\
+    .rename(columns={'CustomerId':'only_road'})
+
+# 都買
+both = df[(df['ProductType'].notnull())&(df['ProductType_road'].notnull())]\
+    .groupby(['FirstTransactionYear','ChannelType']).agg({'CustomerId': 'nunique'})\
+    .rename(columns={'CustomerId':'both'})
+
+# others
+others = df[(df['ProductType'].isnull())&(df['ProductType_road'].isnull())]\
+    .groupby(['FirstTransactionYear','ChannelType']).agg({'CustomerId': 'nunique'})\
+    .rename(columns={'CustomerId':'others'})
+
+result = new_ord.merge(rep_ord,on=['FirstTransactionYear','ChannelType'],)\
+    .merge(only_core,on=['FirstTransactionYear','ChannelType'])\
+    .merge(only_road,on=['FirstTransactionYear','ChannelType']).\
+    merge(both,on=['FirstTransactionYear','ChannelType']).\
+    merge(others,on=['FirstTransactionYear','ChannelType'])
+print(result)
